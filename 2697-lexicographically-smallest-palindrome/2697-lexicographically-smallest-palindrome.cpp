cec34d2a24bf9817c37d58f39000465f84895ea0
krishna6371 <114856230+krishna6371@users.noreply.github.com>
2023-07-22 19:22:32 +0530

    Time: 69 ms (5.02%), Space: 16.2 MB (9.65%) - LeetHub

--- 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     string makeSmallestPalindrome(string s) {
-          string ans=s; // Initialize ans with the original string.
-
-        int i, j;
-        int n = s.length();
-
-        // Loop through the first half of the string.
-        for (i = 0, j = n - 1; i < n / 2; i++, j--) {
-            if (s[i] == s[j]) {
-                // If characters at i and j positions are the same, add them to the answer.
-                ans[i] = s[i];
-                ans[j] = s[j];
-            } else {
-                // If characters are different, select the smaller character and add it to both i and j positions.
-                char c = min(s[i], s[j]);
-                ans[i] = c;
-                ans[j] = c;
+             string ans=s;
+            int i;
+        int j;
+        for(i=0,j=s.length()-1;i<s.length()/2;i++,j--){
+            if(s[i]== s[j]){
+                ans[i]=s[i];
+                ans[j]=s[j];
+                cout<<ans[i]<<endl;
             }
+            else{
+           char c=min(s[i],s[j]);
+            ans[i]=c;
+                ans[j]=c;
+                
+                }
         }
-
-        return ans;
+        return ans;   
     }
 };
